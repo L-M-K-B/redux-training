87cb101265c8c1f2d05f0b2fababb39f03d72b06
Laura <laura@Lauras-Computer.local>
2022-06-16 15:19:55 +0200

    refactor: clean up

---
+++ src/mockdata.js
@@ -0,0 +1,167 @@
+const mockdata = {
+  recipesList: [
+    {
+      _id: '5cf17bb08965831bf2a9bb8f',
+      detailedDescription: [
+        {
+          number: 1,
+          image: '/images/SnickerDoodles01.jpg',
+          explanation: 'Melt the vegan margarine in a small saucepan.',
+        },
+        {
+          number: 2,
+          image: '/images/SnickerDoodles02.jpg',
+          explanation: 'Put it in a mixing bowl and add the cane sugar',
+        },
+        {
+          number: 3,
+          image: '/images/SnickerDoodles03.jpg',
+          explanation:
+            'Beat it until fluffy with a hand mixer or a food processor.',
+        },
+        {
+          number: 4,
+          image: '/images/SnickerDoodles04.jpg',
+          explanation: 'Add vanilla extract.',
+        },
+        {
+          number: 5,
+          image: '/images/SnickerDoodles05.jpg',
+          explanation: 'Add vegetable milk or water. Mix firmly.',
+        },
+        {
+          number: 6,
+          image: '/images/SnickerDoodles06.jpg',
+          explanation:
+            'Sift flour, starch, baking powder and baking soda in a second mixing bowl and mix gently.',
+        },
+      ],
+    },
+    {
+      _id: '5cf17f7f8965831bf2a9bb90',
+      detailedDescription: [
+        {
+          number: 1,
+          image: '/images/joanna-kosinska-340756-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 2,
+          image: '/images/pawel-czerwinski-746610-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 3,
+          image: '/images/scott-webb-51466-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 4,
+          image: '/images/sharon-mccutcheon-528581-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+      ],
+    },
+    {
+      _id: '5cf180218965831bf2a9bb91',
+      detailedDescription: [
+        {
+          number: 1,
+          image: '/images/joanna-kosinska-340756-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 2,
+          image: '/images/pawel-czerwinski-746610-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 3,
+          image: '/images/scott-webb-51466-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 4,
+          image: '/images/sharon-mccutcheon-528581-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+      ],
+    },
+    {
+      _id: '5cf186858965831bf2a9bb93',
+      detailedDescription: [
+        {
+          number: 1,
+          image: '/images/joanna-kosinska-340756-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 2,
+          image: '/images/pawel-czerwinski-746610-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 3,
+          image: '/images/scott-webb-51466-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 4,
+          image: '/images/sharon-mccutcheon-528581-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+      ],
+    },
+    {
+      _id: '5cf180b98965831bf2a9bb92',
+      detailedDescription: [
+        {
+          number: 1,
+          image: '/images/joanna-kosinska-340756-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 2,
+          image: '/images/pawel-czerwinski-746610-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 3,
+          image: '/images/scott-webb-51466-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 4,
+          image: '/images/sharon-mccutcheon-528581-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+      ],
+    },
+    {
+      _id: '5cf18a0e8965831bf2a9bb94',
+      detailedDescription: [
+        {
+          number: 1,
+          image: '/images/joanna-kosinska-340756-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 2,
+          image: '/images/pawel-czerwinski-746610-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 3,
+          image: '/images/scott-webb-51466-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+        {
+          number: 4,
+          image: '/images/sharon-mccutcheon-528581-unsplash.jpg',
+          explanation: 'When in doubt just add sprinkles.',
+        },
+      ],
+    },
+  ],
+};
+export default mockdata;
